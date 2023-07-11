9f0f124149c2a21c6f69d9b1759bd759862496b0
igor <igor.novikov@nemodata.ai>
2023-07-11 12:43:25 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='asuswrt-api',
+    version='0.1.0',
+    packages=['asuswrt'],
+    url='https://github.com/Galaxy83/asuswrt-api',
+    license='MIT',
+    author='Igor Novikov',
+    author_email='',
+    description='AsusWRT API'
+)
